46b4c92902d0b984da418c5fa36bee05dbed3d46
Matt.A-level <isachkin.matt@gmail.com>
2022-05-03 22:57:48 +0300

    Project created

---
+++ TestSN/TestSN/View Controllers/MainVC.swift
@@ -0,0 +1,18 @@
+//
+//  MainVC.swift
+//  TestSN
+//
+//  Created by Matthew  on 03.05.2022.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
---
+++ TestSN/TestSN/View Controllers/MoreDetailVC.swift
@@ -0,0 +1,18 @@
+//
+//  MoreDetailVC.swift
+//  TestSN
+//
+//  Created by Matthew  on 03.05.2022.
+//
+
+import UIKit
+
+class MoreDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
